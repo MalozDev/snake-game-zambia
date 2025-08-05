6bde2e0ced76a8a4efdb2c053a76464bf7d7d440
Stephan Malobeka <malozdev@gmail.com>
2025-08-05 16:58:11 +0200

    Add manifest and icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main style={{ padding: 20 }}>
-      <h1>Welcome to My Mini App</h1>
+      <h1>Welcome to My Mini App Snake game 
+        Zambia
+      </h1>
       <Link href="/snake-game">🎮 Play Snake Game</Link>
     </main>
   );
